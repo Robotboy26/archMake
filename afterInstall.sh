88fa5ecdf2c6c05cdef8858184af5fe51343b2d6
fasttop <Robotboy26>
2024-03-11 22:43:34 -0400

    Changes to be committed:

--- afterInstall.sh
+++ afterInstall.sh
@@ -11,47 +11,21 @@ makepkg -si --noconfirm
 cd ..
 sudo rm -r yay
 
-# Setup gpu drivers
-# Check if NVIDIA GPU is present
-lspci | grep -i nvidia &> /dev/null
-if [ $? -eq 0 ]; then
-    GPU="nvidia"
-fi
-
-# Check if Intel GPU is present
-lspci | grep -i intel &> /dev/null
-if [ $? -eq 0 ]; then
-    GPU="intel"
-fi
-
-# Check if AMD GPU is present
-lspci | grep -i amd &> /dev/null
-if [ $? -eq 0 ]; then
-    GPU="amd"
-fi
-
-# Install open-source drivers based on GPU type using yay with -git versions
-case $GPU in
-    nvidia)
-        yay -S nvidia-git
-        ;;
-    intel)
-        yay -S xf86-video-intel-git
-        ;;
-    amd)
-        yay -S xf86-video-amdgpu-git
-        ;;
-    *)
-        echo "No supported GPU found."
-        ;;
-esac
-
 # Add wanted packages
+
+# Install Wayland for Hyprland
+yay -S wayland weston wlroots --noconfirm
+echo "Wayland install complete."
 
 # Compile hyprland
 yay -S hyprlang-git --noconfirm
 yay -S hyprcursor-git --noconfirm
-yay -S hyprland-git --noconfirm
+
+yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus
+
+# yay -S hyprland-git --noconfirm
+
+echo "Hyprland install complete."
 
 # Hyprland stuff
 
